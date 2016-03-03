cc617df631f8be9c5d90630989b29c793148f8c7
tdiwenocur <diwenocur@ucdavis.edu>
2016-03-03 00:17:49 -0800

    added interactive prototype 3

---
+++ interactive3/js/interactive.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    
+    console.log("ready");
+//show map
+$('img#map').show();
+//hide bus map
+$('img#busmap').hide();
+
+//showing bus map on click of legend
+$('li#busstn').click(function(){
+	$('img#busmap').toggle();
+	$('img#map').toggle();
+		return false;
+})
+
+//dealing with typeahead stuff
+    var substringMatcher=function(strs){
+        return function findMatches(q, cb){
+            var matches, substringRegex;
+            
+            matches = [];
+            
+            substrRegex = new RegExp (q, 'i');
+            
+            $.each(strs, function(i, str){
+                if (substrRegex.test(str)){
+            matches.push(str);
+        
+        
+    }
+    });
+
+    cb(matches);
+        };
+    };
+var states = ['The Silo', 'Memorial Union', 'Cruess', 'Wellman',
+'The Death Star', 'Math-Science Building', 'Student Community Center', 'Giedt', 'Art Building', 'Scilec', 'Physics Building', 'Peter Rock Hall'];
+    
+$('#the-basics .typeahead').typeahead({
+   hint: true,
+   highlight: true,
+   minLength: 1
+},
+  {  
+    name: 'states',
+    source: substringMatcher(states)
+
+});
+
+
+//clicking on zoom
+
+$('#zoomin').click(function(){
+console.log("clicked zoom in");
+	$('#map').animate({
+		width: "900px",
+		opacity: 1,
+	}
+	
+	);
+	$('#yourloc').animate({
+		width: "160px",
+		opacity: 1,
+	}
+	
+	);
+		$('section#legend').animate({
+		left: "1100px",
+		opacity: 1,
+	}
+	
+	);
+		$('form').animate({
+		left: "1100px",
+		opacity: 1,
+	}
+	
+	);
+	
+});
+$('#zoomout').click(function(){
+console.log("clicked zoom out");
+	$('#map').animate({
+		width: "700px",
+		opacity: 1,
+	}
+	
+	);
+	$('#yourloc').animate({
+		width: "139px",
+		opacity: 1,
+	}
+	
+	);
+	$('section#legend').animate({
+		left: "900px",
+		opacity: 1,
+	}
+	
+	);
+	$('form').animate({
+		left: "900px",
+		opacity: 1,
+	}
+	
+	);
+});
+
+
+//close jquery
+})
