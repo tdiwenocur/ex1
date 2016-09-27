2d58961335621029be84b327be29bb3e1091651f
tdiwenocur <diwenocur@ucdavis.edu>
2016-09-26 19:07:47 -0700

    added Re:mind website

---
+++ remind_website/voice_time/interactive.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    
+    console.log("ready");
+    
+
+
+
+//hide the navigation//
+    
+  // $("ul#navbar").css("top","-80px");//
+ $("div#nav").css("top","-150px");   
+    
+    
+
+    
+//hover navigation//
+    $("div#hamburger img").click(function() {
+   $("div#nav").animate({
+        top: "0px",
+         },
+        600
+    );  
+    $("div#hamburger img").hide();
+           
+$("div#nav img").click(function() {
+   $("div#nav").animate({
+        top: "-150px",
+         },
+        600
+    );
+    $("div#hamburger img").show();
+  })
+    
+    
+
+    })
+    
+    
+    
+    
+    	//close jquery
+	})
+	
